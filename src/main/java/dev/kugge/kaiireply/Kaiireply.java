106d5f21a1e89ca378733290af39aec86ff9a9aa
Sofiane H. Djerbi <46628754+kugge@users.noreply.github.com>
2023-04-24 17:14:25 +0300

    Use concurrenthashmap

--- src/main/java/dev/kugge/kaiireply/Kaiireply.java
+++ src/main/java/dev/kugge/kaiireply/Kaiireply.java
@@ -2,15 +2,18 @@ package dev.kugge.kaiireply;
 
 import dev.kugge.kaiireply.command.ReplyCommand;
 
-import dev.kugge.kaiireply.watcher.MsgWatcher;import org.bukkit.Bukkit;import org.bukkit.plugin.java.JavaPlugin;
+import dev.kugge.kaiireply.watcher.MsgWatcher;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Logger;
 
 public class Kaiireply extends JavaPlugin {
 
     public static Logger logger;
-    public static HashMap<String, String> playerRelation = new HashMap<>();
+    public static Map<String, String> playerRelation = new ConcurrentHashMap<>();
 
     @Override
     public void onEnable() {
